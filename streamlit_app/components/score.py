52208b1478aa29b0c156faf21161baa428d12f4d
Ashisheoran <ashisheoran672004@gmail.com>
2026-03-27 12:20:11 +0530

    Removing Streamlit folder from core library structure

--- streamlit_app/components/score.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from utils.ui_helper import score_ring_svg
-
-def render_score(score):
-    st.markdown("### Quality Score")
-    st.markdown(score_ring_svg(score["score"]), unsafe_allow_html=True)
